509ca687a003d0c1b3729337b3d767270e3b8e52
joel.hutson <joel.hutson@cmd.com.au>
2021-10-29 16:16:59 +1100

    removing data

--- s3_policy.tf
+++ s3_policy.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_policy" "bucket_policy_log" {
 
 resource "aws_s3_bucket_policy" "bucket_policy_nolog" {
     count = var.create_logging_bucket ? 0 : 1
-  bucket = aws_s3_bucket.s3_log[0].id
+  bucket = aws_s3_bucket.s3_log.id
   policy = jsonencode(
     {
           Id        = "BUCKET-POLICY"
@@ -56,7 +56,7 @@ resource "aws_s3_bucket_policy" "bucket_policy_nolog" {
 
 resource "aws_s3_bucket_policy" "logging_bucket_policy" {
     count = var.create_logging_bucket ? 1 : 0
-    bucket = aws_s3_bucket.logging_bucket[0].id
+    bucket = aws_s3_bucket.logging_bucket.id
     policy = jsonencode(
     {
           Id        = "BUCKET-POLICY"
