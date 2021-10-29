09236d87dece8e693c326f94e7be742b86167654
joel.hutson <joel.hutson@cmd.com.au>
2021-10-29 16:34:53 +1100

    removing duplication

--- s3.tf
+++ s3.tf
@@ -67,17 +67,6 @@ resource "aws_s3_bucket" "s3_log" {
   }
 }
 
-
-resource "aws_s3_bucket_public_access_block" "s3_Access_log" {
-    count = var.public_access ? 0 : 1
-    bucket = aws_s3_bucket.s3_log[0].id
-    
-    block_public_acls   = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_public_access_block" "s3_Access_nolog" {
     count = var.public_access ? 0 : 1
     bucket = var.bucket_key
