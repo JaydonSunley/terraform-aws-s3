cde141964feb198d3c1abdfc6182513611b2ce71
joel.hutson <joel.hutson@cmd.com.au>
2021-10-29 15:19:15 +1100

    Adding correct descriptions

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "logging_bucket"{
   type        = string
-  description = "(Required) name of the alb"
+  description = "(Required) name of the logging bucket"
 }
 
 variable "bucket_key"{
   type        = string
-  description = "(Required) name of the alb"
+  description = "(Required) name of the s3 bucket"
 }
